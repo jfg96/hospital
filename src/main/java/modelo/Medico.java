d0f6644e36f889ea5da32b8a02caad17266a5607
Carlos Fernández <carlosf.gbeasz@gmail.com>
2026-04-20 17:42:31 +0200

    Creaciones clases Médico y Auxiliar + javadoc de esas clases

---
+++ src/main/java/modelo/Medico.java
@@ -0,0 +1,52 @@
+package modelo;
+
+/**
+ * Clase que representa a un médico en el sistema hospitalario.
+ * @author Carlos Fernández
+ */
+public class Medico extends Trabajador {
+    /**
+     * Especialidad médica del médico.
+     */
+    private String especialidad;
+
+    /**
+     * Constructor que inicializa un médico con los datos especificados.
+     * @param nombre nombre del médico
+     * @param direccion dirección del médico
+     * @param telefono teléfono del médico
+     * @param DNI DNI del médico
+     * @param sueldo sueldo del médico
+     * @param especialidad especialidad médica del médico
+     */
+    public Medico(String nombre, String direccion, String telefono, String DNI, double sueldo, String especialidad) {
+        super(nombre, direccion, telefono, DNI, sueldo);
+        this.especialidad = especialidad;
+    }
+
+    /**
+     * Obtiene la especialidad médica del médico.
+     * @return especialidad del médico
+     */
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    /**
+     * Establece la especialidad médica del médico.
+     * @param especialidad nueva especialidad del médico
+     */
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    /**
+     * Retorna una representación en String del médico.
+     * @return cadena con los datos del médico
+     */
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", Especialidad='" + especialidad + "'}";
+    }
+}
--- src/main/java/modelo/Trabajador.java
+++ src/main/java/modelo/Trabajador.java
@@ -1,68 +1,138 @@
 package modelo;
 
+/**
+ * Clase abstracta que representa a un trabajador en el sistema hospitalario.
+ * @author Carlos Fernández
+ */
 public abstract class Trabajador {
+    /**
+     * Nombre del trabajador.
+     */
     private String nombre;
+    /**
+     * Dirección del trabajador.
+     */
     private String direccion;
+    /**
+     * Teléfono del trabajador.
+     */
     private String telefono;
-    private String DNI;
+    /**
+     * DNI del trabajador.
+     */
+    private String dni;
+    /**
+     * Sueldo del trabajador.
+     */
     private double sueldo;
 
-    public Trabajador(String nombre, String direccion, String telefono, String DNI, double sueldo) {
+    /**
+     * Constructor que inicializa un trabajador con los datos especificados.
+     * @param nombre nombre del trabajador
+     * @param direccion dirección del trabajador
+     * @param telefono teléfono del trabajador
+     * @param dni DNI del trabajador
+     * @param sueldo sueldo del trabajador
+     */
+    public Trabajador(String nombre, String direccion, String telefono, String dni, double sueldo) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
-        this.DNI = DNI;
+        this.dni = dni;
         this.sueldo = sueldo;
     }
 
+    /**
+     * Obtiene el nombre del trabajador.
+     * @return nombre del trabajador
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Establece el nombre del trabajador.
+     * @param nombre nuevo nombre del trabajador
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Obtiene la dirección del trabajador.
+     * @return dirección del trabajador
+     */
     public String getDireccion() {
         return direccion;
     }
 
+    /**
+     * Establece la dirección del trabajador.
+     * @param direccion nueva dirección del trabajador
+     */
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+    /**
+     * Obtiene el teléfono del trabajador.
+     * @return teléfono del trabajador
+     */
     public String getTelefono() {
         return telefono;
     }
 
+    /**
+     * Establece el teléfono del trabajador.
+     * @param telefono nuevo teléfono del trabajador
+     */
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
-    public String getDNI() {
-        return DNI;
+    /**
+     * Obtiene el DNI del trabajador.
+     * @return DNI del trabajador
+     */
+    public String getDni() {
+        return dni;
     }
 
-    public void setDNI(String DNI) {
-        this.DNI = DNI;
+    /**
+     * Establece el DNI del trabajador.
+     * @param dni nuevo DNI del trabajador
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
     }
 
+    /**
+     * Obtiene el sueldo del trabajador.
+     * @return sueldo del trabajador
+     */
     public double getSueldo() {
         return sueldo;
     }
 
+    /**
+     * Establece el sueldo del trabajador.
+     * @param sueldo nuevo sueldo del trabajador
+     */
     public void setSueldo(double sueldo) {
         this.sueldo = sueldo;
     }
 
+    /**
+     * Retorna una representación en String del trabajador.
+     * @return cadena con los datos del trabajador
+     */
     @Override
     public String toString() {
-        return "Trabajador{" +
-                "nombre='" + nombre + '\'' +
-                ", direccion='" + direccion + '\'' +
-                ", telefono='" + telefono + '\'' +
-                ", DNI='" + DNI + '\'' +
-                ", sueldo=" + sueldo +
-                '}';
+        return this.getClass().getSimpleName() + " {" +
+                "DNI='" + dni + '\'' +
+                ", Nombre='" + nombre + '\'' +
+                ", Sueldo=" + sueldo +
+                ", Teléfono='" + telefono + '\'' +
+                ", Dirección='" + direccion + '\'';
     }
 }
