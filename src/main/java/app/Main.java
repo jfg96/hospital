84f19d0f497453777aea191d3d95d9a88748dd3b
Antonio Manuel Rodriguez Palenzuela <kxlx54esp@gmail.com>
2026-04-20 17:06:22 +0200

    Main v1

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,45 @@
+/**
+ *
+ * @author: Antonio Manuel Rodriguez Palenzuela
+ * @since 20/04/2026
+ * @Description:
+ */
+
+//package app;
+//
+//import java.util.Scanner;
+//
+//import static java.awt.Color.GREEN;
+//import static jdk.internal.org.jline.utils.AttributedStyle.WHITE;
+//import static jdk.javadoc.internal.html.HtmlAttr.InputType.RESET;
+//
+//public class Main {
+//    static Scanner input = new Scanner(System.in);
+//    static void main() {
+//        int opcion;
+//
+//
+//        do {
+//            MenuPrincipal();
+//            opcion = input.nextInt();
+//            switch (opcion) {
+//                case 1 -> ejecutarOpcion();
+//                case 2 -> ejecutarOpcion();
+//                case 3 -> ejecutarOpcion();
+//                case 4 -> ejecutarOpcion();
+//                case 5 -> ejecutarOpcion();
+//                case 6 -> ejecutarOpcion();
+//                case 7 -> System.exit(0);
+//            }
+//        } while (opcion == 7);
+//        }
+//    }
+//
+//
+//    static void MenuPrincipal() {
+//        System.out.println("Menu Principal");
+//    }
+//
+//    static void ejecutarOpcion(String nombre) {
+//    System.out.println(GREEN + "\n  ✔  Ejecutando: " + WHITE + nombre + RESET + "\n");
+//    }
